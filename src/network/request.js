3ac18184a5e26c9e1e76dee98019c52d377b1210
linyangxin <1227143051@qq.com>
2020-02-12 23:07:27 +0800

    主页轮播图实现

--- src/network/request.js
+++ src/network/request.js
@@ -7,23 +7,19 @@ export function request(config) {
     timeout:5000
   })
   // 2.1axios请求拦截器
-  //若不return出去，则会被拦截
-  //1、可以对信息做一些处理
-  //2、在界面上显示一个请求图标
-  //3、某些网络请求。必须携带一些特殊的信息
   instance.interceptors.request.use(config=>{
-    console.log(config);
+    // console.log(config);
     return config
   },error => {
-    console.log(err);
+    // console.log(err);
   })
-  //2.2相应拦截,也要return出去
+  //2.2响应拦截,也要return出去
   //1、可以对数据进行处理
   instance.interceptors.response.use(res=>{
-    console.log(res);
+    // console.log(res);
     return res.data
   },err=>{
-    console.log(err);
+    // console.log(err);
   })
 
   //发送真正的网络请求
