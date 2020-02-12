91b07c11cd34850124602f9917fe30e1394342e8
linyangxin <1227143051@qq.com>
2020-02-12 21:15:08 +0800

    将前期编写的tabbar导入

---
+++ src/views/category/Category.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>分类</h2>
+</template>
+
+<script>
+  import Home from "../home/Home";
+  export default {
+    name: "Category",
+    components: {Home}
+  }
+</script>
+
+<style scoped>
+
+</style>
