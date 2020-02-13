36a35089ee5e828f4485038ad06a9222fdcd4867
linyangxin <1227143051@qq.com>
2020-02-13 12:38:58 +0800

    完成home界面的三个分类框

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="goods_item">
+  <img :src="goodsItem.show.img">
+  <div class="goods_info">
+    <p>{{goodsItem.title}}</p>
+    <span class="price">{{goodsItem.price}}</span>
+    <span class="collect">{{goodsItem.cfav}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "GoodsListItem",
+    props:{
+      goodsItem:{
+        type:Object,
+        default(){
+          return{}
+        }
+      }
+    }
+  }
+</script>
+<style scoped>
+  .goods_item {
+    padding-bottom: 40px;
+    position: relative;
+    width: 48%;
+  }
+  .goods_item img {
+    width: 100%;
+    border-radius: 5px;
+  }
+  .goods_info {
+    /* 信息 */
+    font-size: 12px;
+    text-align: center;
+    position: absolute;
+    bottom: 5px;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+  }
+  .goods_info p {
+    /* 标题 */
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    margin-bottom: 3px;
+  }
+  .goods_info .price {
+    /* 价格 */
+    color: var(--color-high-text);
+    margin-right: 20px;
+  }
+  .goods_info .collect{
+    color: var(--color-high-text);
+    position: relative;
+  }
+
+  .goods_info .collect::before {
+    content: "";
+    position: absolute;
+    left: -15px;
+    top: -1px;
+    width: 14px;
+    height: 14px;
+    background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
+  }
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,59 +4,10 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"/>
     <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
+    <tab-control class="tab-control"
+                 :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+
   </div>
 </template>
 
@@ -67,8 +18,9 @@
 
   import NavBar from "components/common/navbar/NavBar";
   import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
 
-  import {getHomeMultidata} from "../../network/home";
+  import {getHomeMultidata, getHomeGoods} from "../../network/home";
 
 
   export default {
@@ -78,20 +30,69 @@
       NavBar,
       HomeSwiper,
       FeatureView,
-      TabControl
+      TabControl,
+      GoodsList
     },
     data(){
       return{
         banners:[],
-        recommends:[]
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'new':{page:0,list:[]},
+          'sell':{page:0,list:[]}
+        },
+        curretType:'pop'
+      }
+    },
+    computed:{
+      showGoods(){
+        return this.goods[this.curretType].list
       }
     },
     created() {
       //1.请求多个数据
+      this.getHomeMultidata()
+
+      //请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods:{
+      /**
+       * 网络请求方法
+       */
+      getHomeMultidata(){
         getHomeMultidata().then(res=>{
           this.banners=res.data.banner.list
           this.recommends=res.data.recommend.list
         })
+      },
+      getHomeGoods(type){
+        const page=this.goods[type].page+1
+        getHomeGoods(type,page).then(res=>{
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page+=1
+        })
+      },
+      /**
+       * 事件监听方法
+       */
+      tabClick(index){
+        switch (index) {
+          case 0:
+            this.curretType='pop'
+            break
+          case 1:
+            this.curretType='new'
+            break
+          case 2:
+            this.curretType='sell'
+            break
+        }
+      }
+
     }
   }
 </script>
@@ -114,5 +115,6 @@
   .tab-control{
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
