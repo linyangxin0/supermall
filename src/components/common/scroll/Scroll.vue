65e4b5daa4c8824925cb7f29820a87006541f1c4
linyangxin <1227143051@qq.com>
2020-02-14 20:04:17 +0800

    优化上拉加载

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -33,15 +33,16 @@
         probeType:this.probeType,
         pullUpLoad: this.pullUpLoad
       })
-      //监听移动
+      //监听滚动位置
       this.scroll.on('scroll',(position)=>{
         this.$emit('scroll',position)
       })
-
-      //监听上拉事件
-      this.scroll.on('pullingUp',()=>{
-        this.$emit('pullingUp')
-      })
+      //监听上拉事件，监听滚动到底部
+      if(this.pullUpLoad){
+        this.scroll.on('pullingUp',()=>{
+          this.$emit('pullingUp')
+        })
+      }
     },
     methods:{
       scrollTo(x,y,time=500) {
