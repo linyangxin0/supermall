598e7bb35fdd49164343d9a5b0d3747dfeefb435
linyangxin <1227143051@qq.com>
2020-02-15 09:59:34 +0800

    休息一下

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,24 +2,28 @@
 <div id="detail">
   <DetailNavBar></DetailNavBar>
   <detail-swiper :top-images="topImages"></detail-swiper>
+  <detail-base-info :goods="goods"></detail-base-info>
 </div>
 </template>
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar";
-  import {getDetail} from "network/detail";
+  import {getDetail,Goods} from "network/detail";
   import DetailSwiper from "./childComps/DetailSwiper";
+  import DetailBaseInfo from "./childComps/DetailBaseInfo";
 
   export default {
     name: "detail",
     components:{
+      DetailBaseInfo,
       DetailNavBar,
       DetailSwiper
     },
     data(){
       return{
         iid:null,
-        topImages:[]
+        topImages:[],
+        goods:null
       }
     },
     created() {
@@ -29,8 +33,17 @@
       //请求数据
       getDetail(this.iid).then(res=>{
         console.log(res);
-        this.topImages=res.result.itemInfo.topImages
+        const data=res.result
+        //轮播图数据
+        this.topImages=data.itemInfo.topImages
+
+        //获取商品信息
+        this.goods=new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+
       })
+
+
+
     }
   }
 </script>
---
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <div class="detail-base-info-title">{{goods.title}}</div>
+    </div>
+    <div class="detail-base-info-price">
+      <div class="detail-base-info-newprice">{{goods.newPrice}}</div>
+      <span><s>{{goods.oldPrice}}</s></span>
+      <div class="detail-base-info-discount">{{goods.discount}}</div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    name: "DetailBaseInfo",
+    props:{
+      goods:{
+        type:Object,
+        default(){
+          return{}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .detail-base-info-title{
+    margin-top: 10px;
+    margin-left: 5px;
+    margin-right: 5px;
+    font-size: 16px;
+    color: black;
+  }
+
+  .detail-base-info-price{
+    display: flex;
+    margin-top: 10px;
+  }
+
+  .detail-base-info-newprice{
+    color: var(--color-high-text);
+    font-size: 20px;
+    margin-left: 10px;
+  }
+
+  .detail-base-info-price s{
+    font-size: 12px;
+    color: #999999;
+    margin-left: 8px;
+    margin-bottom: 0;
+  }
+
+  .detail-base-info-discount{
+    background-color: #ff5777;
+    color: white;
+    font-size: 12px;
+    margin: 3px;
+    border-radius: 25px;
+  }
+</style>
