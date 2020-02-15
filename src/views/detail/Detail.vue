ed4efb56672e349e8faaad1b5edcea59a906669e
linyangxin <1227143051@qq.com>
2020-02-15 20:13:33 +0800

    Detail页面基本完成

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -5,58 +5,7 @@
     <detail-swiper :top-images="topImages"></detail-swiper>
     <detail-base-info :goods="goods"></detail-base-info>
     <detail-shop-info :shop="shop"></detail-shop-info>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>11</li>
-      <li>12</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>18</li>
-      <li>19</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>25</li>
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-      <li>30</li>
-      <li>31</li>
-      <li>32</li>
-      <li>33</li>
-      <li>34</li>
-      <li>35</li>
-      <li>36</li>
-      <li>37</li>
-      <li>38</li>
-      <li>39</li>
-      <li>40</li>
-      <li>41</li>
-      <li>42</li>
-      <li>43</li>
-      <li>44</li>
-      <li>45</li>
-      <li>46</li>
-      <li>47</li>
-      <li>48</li>
-      <li>49</li>
-      <li>50</li>
-    </ul>
+    <detail-goods-info :detail-info="detailInfo"/>
   </scroll>
 
 </div>
@@ -69,6 +18,8 @@
   import DetailBaseInfo from "./childComps/DetailBaseInfo";
   import DetailShopInfo from "./childComps/DetailShopInfo";
   import Scroll from "components/common/scroll/Scroll";
+  import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+
   export default {
     name: "detail",
     components:{
@@ -76,14 +27,16 @@
       DetailNavBar,
       DetailSwiper,
       DetailShopInfo,
-      Scroll
+      Scroll,
+      DetailGoodsInfo
     },
     data(){
       return{
         iid:null,
         topImages:[],
         goods:{},
-        shop:{}
+        shop:{},
+        detailInfo:{}
       }
     },
     created() {
@@ -102,9 +55,10 @@
 
         //创建店铺信息
         this.shop=new Shop(data.shopInfo)
+
+        //保存商品详情数据
+        this.detailInfo=data.detailInfo
       })
-
-
 
     }
   }
---
+++ src/views/detail/childComps/DetailGoodsInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="detail-goods-info" v-if="Object.keys(detailInfo).length !== 0">
+    <div class="top">
+      {{detailInfo.desc}}
+    </div>
+    <div class="center">
+      {{detailInfo.detailImage[0].key}}
+    </div>
+    <div  class="detail-goods-images">
+        <img v-for="(item,index) in detailInfo.detailImage[0].list" :src="item">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DetailGoodsInfo",
+    props:{
+      detailInfo:{
+        type:Object
+      }
+    },
+
+  }
+</script>
+
+<style scoped>
+
+  .detail-goods-info{
+    border-top: 1px solid rgba(100,100,100,.1);
+    margin-top: 15px;
+    padding-top: 15px;
+  }
+
+  .detail-goods-info .top{
+    padding-bottom: 15px;
+    border-bottom: 1px solid rgba(100,100,100,.1);
+  }
+
+  .detail-goods-info .center{
+    padding-top: 15px;
+    color: black;
+  }
+
+  .detail-goods-images img{
+    width: 100%;
+  }
+</style>
