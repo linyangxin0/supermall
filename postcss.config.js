d6b34f187a6bf37619a8c8fd5d5d8bacee265f85
linyangxin <1227143051@qq.com>
2020-02-17 20:40:14 +0800

    postcss-px-to-viewport配置postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    "postcss-ps-to-viewport":{
+    autoprefixer: {browsers: 'last 5 version'},
+    "postcss-px-to-viewport":{
       viewportWidth: 375,
       viewportHeight: 667,
       unitPrecision: 5,
       viewportUnit: 'vw',
       selectorBlackList: ['ignore','tab-bar','tab-bar-item'],
       minPixelValue: 1,
-      mediaQuery: false
+      mediaQuery: false,
+      exclude:[/TabBar/]
     }
   }
 }
